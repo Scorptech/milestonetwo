1262978b1edde0d43f7dc4e7527f4f280af6b41a
Scorptech <scorptech@eircom.net>
2019-11-12 14:08:27 +0000

    JS script for sending email

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,19 @@
+function sendMail(contactForm) {
+    emailjs.send("blacknight", "template_X510oNUx", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "information_request": contactForm.requestsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+            alert("SUCCESS - Your Message Has Been Sent");
+        },
+        function(error) {
+            console.log("FAILED", error);
+            alert("FAILED - Your Message Has Not Been Sent");
+        }
+    );
+    return false;  // To block from loading a new page
+}
+
