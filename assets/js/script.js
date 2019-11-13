c3a9d778d77d865f5284a5cfb8e12639ae068210
Scorptech <scorptech@eircom.net>
2019-11-13 15:54:07 +0000

    added comments to file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,17 @@ var ctx = document.getElementById('impact_bar').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['2006-QV89', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['2006 QV89', '2010 UJ', '2012 TC4', '2010 HP20', '2009 FG', '2015 DQ224', '2001 BA16'],
         datasets: [{
-            label: '# Asteroids and Near Earth Location',
-            data: [2041, 2042, 2043, 2045, 2046, 2047],
+            label: '# Known Asteroids and Period Start',
+           data: [2019, 2020, 2020, 2021, 2022, 2029, 2033],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
+                'rgba(151, 187, 205, 0.2)',
                 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
@@ -21,6 +22,7 @@ var myChart = new Chart(ctx, {
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
+                'rgba(151, 187, 205, 1)',
                 'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
@@ -31,9 +33,9 @@ var myChart = new Chart(ctx, {
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero: true
+                    beginAtZero: false
                 }
-            }]
+            }],
         },
             layout: {
             padding: {
@@ -51,16 +53,16 @@ var ctx = document.getElementById('impact_pie').getContext('2d');
 var myPieChart = new Chart(ctx, {
     type: 'pie',
     data: {
-        labels: ['2006-QV89', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['2006 QV89', '2010 UJ', '2012 TC4', '2010 HP20', '2009 FG', '2015 DQ224', '2001 BA16'],
         datasets: [{
-            label: '# Asteroids and Near Earth Location',
-            data: [2041, 2042, 2043, 2045, 2046, 2047],
+            data: [0.0076, 0.00061, 0.0085, 0.0041, 0.00080, 0.0068, 0.00098],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
+                'rgba(151, 187, 205, 0.2)',
                 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
@@ -69,6 +71,7 @@ var myPieChart = new Chart(ctx, {
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
+                'rgba(151, 187, 205, 1)',
                 'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
@@ -79,8 +82,8 @@ var myPieChart = new Chart(ctx, {
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero: true
-                }
+                    beginAtZero: false
+                    }
             }]
         },
         layout: {
@@ -89,7 +92,30 @@ var myPieChart = new Chart(ctx, {
                 right: 20,
                 top: 0,
                 bottom: 0
-            }}
+            }},
+            legend: {
+            position: "bottom"
+            },
+            title: {
+            display: true,
+            text: '% Asteriod Impact Possibility'
+        },
+        scales: {
+        xAxes: [{
+            gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+            }
+        }],
+        yAxes: [{
+            gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+            },
+            ticks: {
+                display: false
+            }
+          }]
+      
+        }  
     }
 });
 
@@ -101,12 +127,12 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['2006 QV89', '2010 UJ', '2012 TC4', '2010 HP20', '2009 FG', '2015 DQ224', '2001 BA16'],
         datasets: [{
-            label: 'My First dataset',
+            label: '# Velocity (speed in km/s) of Asteroid',
             backgroundColor: 'rgb(54, 162, 235, 0.2)',
             borderColor: 'rgb(54, 162, 235)',
-            data: [45, 30, 20, 2, 5, 10, 0]
+            data: [5.16, 2.84, 6.51, 7.66, 9.12, 14.59, 4.9]
         }]
     },
 
@@ -131,6 +157,55 @@ var chart = new Chart(ctx, {
 });
 
 
+var ctx = document.getElementById('impact_hozbar').getContext('2d');
+var myBarChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: ['2006 QV89', '2010 UJ', '2012 TC4', '2010 HP20', '2009 FG', '2015 DQ224', '2001 BA16'],
+        datasets: [{
+            label: '# Asteroid Magnitude - Estimated Diameter (km)',
+            data: [25.3, 26.2, 26.7, 28.2, 25.5, 29.4, 25.8],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(151, 187, 205, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(151, 187, 205, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        layout: {
+            padding: {
+                left: 20,
+                right: 20,
+                top: 0,
+                bottom: 0
+            }}
+    }
+});
+
+
+/*
 var ctx = document.getElementById('impact_scatter').getContext('2d');
 
 var scatterChart = new Chart(ctx, {
@@ -168,3 +243,11 @@ var scatterChart = new Chart(ctx, {
             }}
     }
 });
+
+I did some quick investigation and found the workaround:
+myLineChart.scale.xLabels = ["label1", "label2", "label3"];
+myLineChart.update();
+
+
+
+*/
